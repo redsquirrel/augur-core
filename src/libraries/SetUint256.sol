7d938538a60737a2366a6de08c0e20fa3c7d3365
janani-sridhar <janani.sridhar@yahoo.com>
2017-08-04 14:00:38 -0700

    Fixed some more indentation.

--- src/libraries/SetUint256.sol
+++ src/libraries/SetUint256.sol
@@ -5,37 +5,37 @@ import "ROOT/legacy_reputation/Ownable.sol";
 
 contract IterableMapUint256 is DelegationTarget, Ownable {
 
-	mapping(uint256 => bool) private collection;
-	uint private count;
-	address private owner;
-	bool private initialized;
+    mapping(uint256 => bool) private collection;
+    uint private count;
+    address private owner;
+    bool private initialized;
 
-    function initialize(address _owner) public returns (bool) onlyOwner {
+    function initialize(address _owner) public onlyOwner returns (bool) {
         require(!initialized);
         initialized = true;
         owner = _owner;
         return (true);
     }
 
-    function addSetItem(uint256 _item) public returns (bool) onlyOwner {
-    	require(!contains(_item));
-    	collection[_item] = true;
-    	count += 1;
-    	return (true);
+    function addSetItem(uint256 _item) public onlyOwner returns (bool) {
+        require(!contains(_item));
+        collection[_item] = true;
+        count += 1;
+        return (true);
     }
 
-    function remove(uint256 _item) public returns (bool) onlyOwner {
-    	require(contains(_item));
-    	delete collection[_item];
-    	count -= 1;
-    	return (true)
+    function remove(uint256 _item) public onlyOwner returns (bool) {
+        require(contains(_item));
+        delete collection[_item];
+        count -= 1;
+        return (true)
     }
 
-    function contains(uint256 _item) public returns (bool) constant {
-    	return (collection[_item]);
+    function contains(uint256 _item) public constant returns (bool) {
+        return (collection[_item]);
     }
 
-    function count() public returns (uint256) constant {
-    	return (count);
+    function count() public constant returns (uint256) {
+        return (count);
     }
-}	
+}   
