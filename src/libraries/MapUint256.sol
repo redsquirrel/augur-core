85503f389ee1e88968159d83906e0fe2ffd3624f
janani-sridhar <janani.sridhar@yahoo.com>
2017-08-04 13:36:40 -0700

    Fixed mixed indentation with tabs and spaces.

--- src/libraries/MapUint256.sol
+++ src/libraries/MapUint256.sol
@@ -5,16 +5,16 @@ import "ROOT/legacy_reputation/Ownable.sol";
 
 contract MapUint256 is DelegationTarget, Ownable {
 
-	struct MapItem {
-		bool hasValue;
-		uint256 value;
-	}
+    struct MapItem {
+        bool hasValue;
+        uint256 value;
+    }
 
-	mapping(uint256 => MapItem) private collection;
-	address private owner;
-	bool private initialized;
-	uint256 private count;
-	
+    mapping(uint256 => MapItem) private collection;
+    address private owner;
+    bool private initialized;
+    uint256 private count;
+    
 
     function initialize(address _owner) public returns (bool) onlyOwner {
         require(!initialized);
@@ -24,35 +24,35 @@ contract MapUint256 is DelegationTarget, Ownable {
     }
 
     function addMapItem(uint256_key, uint256_value) public returns (bool) onlyOwner {
-    	require(!contains(_key));
-    	collection[_key].hasValue = true;
-    	collection[_key].value = _value;
-    	count += 1;
-    	return (true);
+        require(!contains(_key));
+        collection[_key].hasValue = true;
+        collection[_key].value = _value;
+        count += 1;
+        return (true);
     }
 
     function remove(uint256 _key) public returns (bool) onlyOwner {
-    	require(contains(_key));
-    	delete collection[_key].hasValue;
-    	delete collection[_key].value;
-    	count -= 1;
-    	return (true);
+        require(contains(_key));
+        delete collection[_key].hasValue;
+        delete collection[_key].value;
+        count -= 1;
+        return (true);
     }
 
     function contains(uint256 _key) public returns (bool) constant {
-    	return(collection[_key].hasValue); 
+        return(collection[_key].hasValue); 
     } 
 
     function getValueOrZero(uint256 _key) public returns (uint256) constant {
-    	return(collection[_key].value);
+        return(collection[_key].value);
     }
 
     function getValue(uint256 _key) public returns (uint256) constant {
-    	require(contains(_key));
-    	return(collection[_key].value);
+        require(contains(_key));
+        return(collection[_key].value);
     }
 
     function count() public returns (uint256) constant {
-    	return (count);
+        return (count);
     }
 }
