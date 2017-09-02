323fc26a45c6d37a1bb548717f809000cbb72194
Dave Hoover <dave.hoover@gmail.com>
2017-09-02 14:37:11 -0500

    Making CancelOrder.sol happy with @MicahZoltu's interface pattern

--- src/trading/CancelOrder.sol
+++ src/trading/CancelOrder.sol
@@ -6,8 +6,7 @@ pragma solidity ^0.4.13;
 
 import 'ROOT/Controller.sol';
 import 'ROOT/libraries/ReentrancyGuard.sol';
-import 'ROOT/libraries/Trading.sol';
-import 'ROOT/trading/Cash.sol';
+import 'ROOT/trading/Trading.sol';
 import 'ROOT/trading/Orders.sol';
 
 
@@ -20,7 +19,7 @@ contract CancelOrder is Controlled, ReentrancyGuard {
      * @dev Cancellation: cancels an order, if a bid refunds money, if an ask returns shares
      * @return true if successful; throw on failure
      */
-    function cancelOrder(bytes32 _orderId, Trading.TradeTypes _type, Market _market, uint8 _outcome) nonReentrant external returns (bool) {
+    function cancelOrder(bytes32 _orderId, Trading.TradeTypes _type, IMarket _market, uint8 _outcome) nonReentrant external returns (bool) {
         require(_orderId != bytes32(0));
         require(_market.getTypeName() == "Market");
 
@@ -45,7 +44,7 @@ contract CancelOrder is Controlled, ReentrancyGuard {
     /**
      * @dev Issue refunds
      */
-    function refundOrder(address _sender, Trading.TradeTypes _type, uint256 _fxpSharesEscrowed, uint256 _fxpMoneyEscrowed, Market _market, uint8 _outcome) private returns (bool) {
+    function refundOrder(address _sender, Trading.TradeTypes _type, uint256 _fxpSharesEscrowed, uint256 _fxpMoneyEscrowed, IMarket _market, uint8 _outcome) private returns (bool) {
         if (_fxpSharesEscrowed > 0) {
             // Return to user sharesEscrowed that weren't filled yet for all outcomes except the order outcome
             if (_type == Trading.TradeTypes.Bid) {
@@ -65,7 +64,7 @@ contract CancelOrder is Controlled, ReentrancyGuard {
 
         // Return to user moneyEscrowed that wasn't filled yet
         if (_fxpMoneyEscrowed > 0) {
-            Cash _denominationToken = _market.getDenominationToken();
+            ICash _denominationToken = _market.getDenominationToken();
             require(_denominationToken.transferFrom(_market, _sender, _fxpMoneyEscrowed));
         }
 
